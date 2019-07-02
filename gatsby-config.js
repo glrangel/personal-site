b4127c87c3b8648bf9272bc08b17a2cb07576a5b
Gabriel Rangel <glrangel25@gmail.com>
2019-07-02 14:53:48 -0700

    Fix unstyled components on load

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,9 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
-    "gatsby-plugin-typescript",
     {
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        google: {
-          families: ['Roboto Mono','VT323','Major Mono Display','Cutive Mono', 'Source Code Pro']
-        }
-      }
-    }
+     resolve: `gatsby-plugin-styled-components`
+     }
+
   ]
 }
